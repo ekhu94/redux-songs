8c352acdbcf71dbb3d22e0ecc6af793efee2f7c1
Erik Huang <ekhu94@gmail.com>
2021-03-23 08:52:30 -0700

    import action creator selectSong and pass in as 2nd argument to connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import SongList from './SongList';
 
-import { selectSong } from '../actions';
-
 const App = () => {
     return (
         <div className="ui container grid">
